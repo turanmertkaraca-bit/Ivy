a7542a5734dd14d4d04c9445cf9b6ed3ea72dc1f
turanmertkaraca-bit <turanmertkaraca@gmail.com>
2026-07-13 14:19:52 +0300

    Fix KSP compilation error: update Kotlin and KSP to compatible versions

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.4.0" apply false
+    id("com.android.library") version "8.4.0" apply false
+    kotlin("android") version "2.0.0" apply false
+    kotlin("jvm") version "2.0.0" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
